6c4704e33ffba33f6065d7be775cf659e8ea4697
aarya moktan <aaryamoktan2055@gmail.com>
2023-09-21 02:26:13 -0700

    commit main

--- src/components/video.js
+++ src/components/video.js
@@ -6,12 +6,7 @@ function Video(props)
     return (
         <>
             <div className="apple">
-               <form onClick={submit}>
-                <input type="text" name="name" id="name"  value={data.name} onChange={onchange} placeholder="name"/>
-                <input type="text" name="email" value={data.email} onChange={onchange} placeholder="email"/>
-                <input type="text" name="age"   value={data.age} onChange={onchange} placeholder="age"/>
-                <button type="submit">Submit</button>
-               </form>   
+               
            </div>
         </>
     )
